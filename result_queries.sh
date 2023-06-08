727456272df871810803992f2d46703127675ac4
Jerry <jerrypeng02@gmail.com>
2023-06-08 09:59:36 -0700

    make result_queries.sh a automatically script and changed the description of README

--- result_queries.sh
+++ result_queries.sh
@@ -1,5 +1,1 @@
-docker exec -it takehome_db psql -U postgres
-\c takehomedb;
-SELECT * FROM average_experiments;
-SELECT * FROM most_commonly_experimented_compound;
-SELECT * FROM total_experiments;
+docker exec -it takehome_db psql -U postgres -d takehomedb -c 'SELECT * FROM total_experiments, average_experiments, most_commonly_experimented_compound'
